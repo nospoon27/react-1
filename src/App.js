79f92e51eaadc17877b649f6e5bf16db6ee8ec9c
Dim Zinnatov <fr.easy27@outlook.com>
2020-02-19 18:21:05 +0300

    navlink error

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ function App() {
         <Header/>
         <Navbar/>
         <div className="app-wrapper-content">
-          <Route path='/dialogs' component={Dialogs}/>
           <Route path='/profile' component={Profile}/>        
-          <Route path='/news' component={News}/>        
+          <Route path='/dialogs' component={Dialogs}/>
+          {/* <Route path='/news' component={News}/>        
           <Route path='/music' component={Music}/>        
-          <Route path='/settings' component={Settings}/>        
+          <Route path='/settings' component={Settings}/>         */}
         </div>
       </div>
     </BrowserRouter>
