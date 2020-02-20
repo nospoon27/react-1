24143ada2e3cbb0e7524910e72284f6e7d82b692
Dim Zinnatov <fr.easy27@outlook.com>
2020-02-20 11:16:30 +0300

    21 урок

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import s from './Dialogs.module.css';
 
-function Dialogs(){
-   return(
-      <div>Dialogs</div>
-   );
+function Dialogs() {
+  return (
+    <div>
+      <div className={s.dialogs}>
+        <div className={s.dialogsItem}>
+          <div className={s.dialog}>Дим</div>
+          <div className={s.dialog}>Сергей</div>
+          <div className={s.dialog}>Игорь</div>
+          <div className={s.dialog}>Евгений</div>
+        </div>
+        <div className={s.messages}>
+          <div className={s.message}>Hi</div>
+          <div className={s.message}>How is your</div>
+          <div className={s.message}>Bye</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Dialogs;
