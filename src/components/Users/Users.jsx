03b2354ec102345a263f92fefd58444e05c000d4
Dim Zinnatov <fr.easy27@outlook.com>
2020-05-13 20:48:03 +0300

    62 follow/unfollow

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,25 +1,22 @@
-import React from 'react'
+import React from "react";
 import styles from "./Users.module.css";
 import userPhoto from "./../../assets/images/userPhoto1.png";
-
+import axios from "axios";
 
 let Users = (props) => {
+  let pageCount = Math.ceil(props.totalUsersCount / props.pageSize);
+  let pages = [];
+  for (let i = 1; i <= pageCount; i++) {
+    pages.push(i);
+  }
 
-   let pageCount = Math.ceil(props.totalUsersCount / props.pageSize);
-   let pages = [];
-   for (let i = 1; i <= pageCount; i++) {
-     pages.push(i);
-   }
-
-   return (
-   <div>
+  return (
+    <div>
       <div className={styles.pagination}>
         {pages.map((page) => {
           return (
             <span
-              className={
-                props.currentPage === page && styles.selectedPage
-              }
+              className={props.currentPage === page && styles.selectedPage}
               onClick={(e) => {
                 props.onPageChanged(page);
               }}>
@@ -32,22 +29,50 @@ let Users = (props) => {
         <div key={u.id}>
           <span>
             <div>
-              <img src={u.photos.small != null ? u.photos.small : userPhoto}
-                   width="80px"
-                   className={styles.userPhoto}/>
+              <img
+                src={u.photos.small != null ? u.photos.small : userPhoto}
+                width="80px"
+                className={styles.userPhoto}
+              />
             </div>
             <div>
               {u.followed ? (
                 <button
                   onClick={() => {
-                    props.unfollow(u.id);
+                    axios
+                      .delete(
+                        `https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                          withCredentials: true,
+                          headers: {
+                            "API-KEY": "6d6a5255-b379-4b78-9ee2-ec32179f3020"
+                          }
+                        }
+                      )
+                      .then((responce) => {
+                        if (responce.data.resultCode === 0) {
+                          props.unfollow(u.id);                         
+                        }
+                      });
                   }}>
                   unfollow
                 </button>
               ) : (
                 <button
                   onClick={() => {
-                    props.follow(u.id);
+                    axios
+                      .post(
+                        `https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                          withCredentials: true,
+                          headers: {
+                            "API-KEY": "6d6a5255-b379-4b78-9ee2-ec32179f3020"
+                          }
+                        }
+                      )
+                      .then((responce) => {
+                        if (responce.data.resultCode === 0) {
+                          props.follow(u.id);                         
+                        }
+                      });
                   }}>
                   follow
                 </button>
@@ -67,7 +92,7 @@ let Users = (props) => {
         </div>
       ))}
     </div>
-    );
-}
+  );
+};
 
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -12,7 +12,10 @@ class UsersContainer extends React.Component {
   //* Как только компонент отрисовался
   componentDidMount() {
     axios.get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`
+        `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize},`,
+        {
+          withCredentials: true
+        }
       )
       .then((responce) => {
         this.props.setUsers(responce.data.items);
@@ -24,7 +27,10 @@ class UsersContainer extends React.Component {
     this.props.setCurrentPage(pageNumber);
     axios
       .get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`,
+        {
+          withCredentials: true
+        }
       )
       .then((responce) => {
         this.props.setUsers(responce.data.items);
